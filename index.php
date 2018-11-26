49f18a1cdb3fa57b471f19519a213ec6d81d9627
Lorne Barnaby <lorne123barnaby@gmail.com>
2018-11-26 17:42:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html>
-<body>
-<p>This is a 50 by 50 maze generated with prims algorithm</p>
 <?php
 class Cell{
     Public $X;
     Public $Y;
     Public $isWall;
+    Public $id;
 
     function __construct($xPos, $yPos){
         $this->X = $xPos;
@@ -40,6 +37,10 @@ class Cell{
         }
         unset($c);
         return $counter;
+    }
+
+    function setId($n){
+        $this->id = $n;
     }
 }
 ?>
@@ -113,11 +114,13 @@ foreach($neib as &$c){
     array_push($myGrid->Walls, $c);
 }
 unset($c);
-
+$cellNumber = 0;
 while (count($myGrid->Walls) > 0){
     $thisWall = $myGrid->getRandomWall();
     if($thisWall[0]->DividesHowMany($myGrid->Cells) == 1){
         $myGrid->Cells[$thisWall[0]->X][$thisWall[0]->Y]->makePath();
+        $myGrid->Cells[$thisWall[0]->X][$thisWall[0]->Y]->setId($cellNumber);
+        $cellNumber++;
         $walls = $myGrid->getNeighbours($myGrid->Cells[$thisWall[0]->X][$thisWall[0]->Y]);
         foreach($walls as &$c){
             array_push($myGrid->Walls, $c);
@@ -131,20 +134,77 @@ while (count($myGrid->Walls) > 0){
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    .Invisible{
+        -webkit-animation-fill-mode: forwards; /* Chrome 16+, Safari 4+ */
+        -moz-animation-fill-mode: forwards;    /* FF 5+ */
+        -o-animation-fill-mode: forwards;      /* Not implemented yet */
+        -ms-animation-fill-mode: forwards;     /* IE 10+ */
+        animation-fill-mode: forwards;  
+        -webkit-animation-name: example; /* Safari 4.0 - 8.0 */
+        -webkit-animation-duration: .1s; /* Safari 4.0 - 8.0 */   
+        animation-name: example;
+        animation-duration: 1s;
+    }
+
+
+
+    /* Safari 4.0 - 8.0 */
+    @-webkit-keyframes example {
+        from {opacity: 0;}
+        to {opacity: 1;}
+    }
+
+    /* Standard syntax */
+    @keyframes example {
+        from {opacity: 0;}
+        to {opacity: 1;}
+    }
+</style>
+<script>
+            function animateThatStuff() {
+                let max = <?php echo (string)$cellNumber; ?>;
+                for(let i = 0; i < max; i++){
+                    let x = document.getElementsByClassName(i.toString());
+                    console.log(x);
+                    x[0].style.opacity = "0";
+                }  
+                console.log("works");
+                for(let i = 0; i < max; i++){
+                    let y = document.getElementsByClassName(i.toString());
+                    y[0].style.animationDelay = (i/10).toString().concat("s");
+                    y[0].classList.add("Invisible");
+                }  
+               
+            };
+
+</script>
+</head>
+<body>
+
+<p>This is a 50 by 50 maze generated with prims algorithm. Click the maze to watch it generate</p>
+<p> source code at <a href="https://github.com/LorneBarnaby/maze.lorneb.com">My Github</a></p><br>
+<br>
+
 
 
 <?php 
-    echo "<svg width=\"". (string)(15 *  $myGrid->Width) . "\"";
+    echo "<svg onclick=\"animateThatStuff()\" width=\"". (string)(15 *  $myGrid->Width) . "\"";
     echo " height=\"". (string)(15 *  $myGrid->Height) . "\">";
     for($i = 0; $i <= $myGrid->Height; $i++){
         for($j = 0; $j <= $myGrid->Height; $j++){   
             echo "<rect width=\"15\" height=\"15\"";
-            echo " x=\"" . (string)(($j * 15) - 1) . "\"";
-            echo " y=\"" . (string)(($i * 15) - 1). "\"";
+            echo " x=\"" . (string)(($j * 15)) . "\"";
+            echo " y=\"" . (string)(($i * 15)). "\"";
             if($myGrid->Cells[$i][$j]->isWall == FALSE){
                 echo " fill=\"black\"";
+                echo " class=\"" . (string)$myGrid->Cells[$i][$j]->id . "\"";
             } else {
                 echo " fill=\"white\"";
+                echo " class=\"white\"";
             }
             echo "/>";
         }
